a49762941397b992d42db0a33040462cdfba4968
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2019-12-02 15:45:23 +0000

    Added methods (wip)

--- core/lib/repositories/document_repository.dart
+++ core/lib/repositories/document_repository.dart
@@ -1,28 +1,125 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flash_chat_core/utils/secure_storage_utils.dart';
+import 'package:flash_chat_core/utils/rsa_utils.dart';
+import 'package:pointycastle/export.dart';
 
 /// The document repository interface
-abstract class IDocumentRepository {}
+abstract class IDocumentRepository {
+  /// Returns the messages
+  Stream<QuerySnapshot> getMessageStream(
+      {String collection, String orderBy, bool descending});
+
+  /// Returns the users public keys
+  Future<Map<String, String>> getUsersPublicKeys();
+
+  /// Posts a message
+  Future<void> postMesssage(
+      {String message, String senderEmail, RSAPrivateKey privateKey});
+
+  /// Post a user's public key
+  Future<void> postUserPublicKey({String email, String publicKeyPem});
+}
 
 /// The document repository implementation
 class DocumentRepository extends IDocumentRepository {
-  /// Construcs the document repository
-  DocumentRepository(this._firestore, this._secureStorageUtils)
-      : assert(_firestore != null),
-        assert(_secureStorageUtils != null);
+  /// Constructs the document repository
+  DocumentRepository(this._firestore) : assert(_firestore != null);
 
   final Firestore _firestore;
 
-  final SecureStorageUtils _secureStorageUtils;
+  @override
+  Stream<QuerySnapshot> getMessageStream(
+      {String collection, String orderBy, bool descending}) {
+    if (collection == null && collection == '') {
+      throw ArgumentError('collection must not be null or empty');
+    }
 
-  Future<void> _addPublicKeyToFirestore({String email}) async {
-    final publicKeyPem = await _secureStorageUtils.getPublicKey(email);
+    if (orderBy == null && orderBy == '') {
+      throw ArgumentError('orderBy must not be null or empty');
+    }
 
-    final data = <String, String>{
-      'sender': email,
-      'publicKey': publicKeyPem,
-    };
+    if (descending == null) {
+      throw ArgumentError('descending must not be null or empty');
+    }
 
-    await _firestore.collection('publicKeys').add(data);
+    try {
+      return _firestore
+          .collection(collection)
+          .orderBy(orderBy, descending: descending)
+          .snapshots();
+    } on Object catch (e) {
+      throw 'Get Message Stream Failed: $e';
+    }
+  }
+
+  @override
+  Future<Map<String, String>> getUsersPublicKeys() async {
+    try {
+      Map<String, String> users;
+
+      final querySnapshot =
+          await _firestore.collection('publicKeys').getDocuments();
+
+      if (querySnapshot != null) {
+        for (var document in querySnapshot.documents) {
+          users[document.data['sender']] = document.data['publicKey'];
+        }
+      }
+
+      return users;
+    } on Object catch (e) {
+      throw 'Get Users Failed: $e';
+    }
+  }
+
+  @override
+  Future<void> postMesssage(
+      {String message, String senderEmail, RSAPrivateKey privateKey}) async {
+    if (message == null && message == '') {
+      throw ArgumentError('message must not be null or empty');
+    }
+
+    if (senderEmail == null && senderEmail == '') {
+      throw ArgumentError('senderEmail must not be null or empty');
+    }
+
+    if (privateKey == null) {
+      throw ArgumentError('privateKey must not be null');
+    }
+
+    try {
+      final encryptedMessage = encryptMessage(message, privateKey);
+
+      final data = <String, Object>{
+        'message': encryptedMessage,
+        'sender': senderEmail,
+        'timestamp': DateTime.now().millisecondsSinceEpoch
+      };
+
+      await _firestore.collection('messages').add(data);
+    } on Object catch (e) {
+      throw 'Post Message Failed: $e';
+    }
+  }
+
+  @override
+  Future<void> postUserPublicKey({String email, String publicKeyPem}) async {
+    if (email == null && email == '') {
+      throw ArgumentError('email must not be null or empty');
+    }
+
+    if (publicKeyPem == null && publicKeyPem == '') {
+      throw ArgumentError('password must not be null or empty');
+    }
+
+    try {
+      final data = <String, String>{
+        'sender': email,
+        'publicKey': publicKeyPem,
+      };
+
+      await _firestore.collection('publicKeys').add(data);
+    } on Object catch (e) {
+      throw 'Post User Failed: $e';
+    }
   }
 }
