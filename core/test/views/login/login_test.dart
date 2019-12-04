2de90d18da4b53a4131000bae568c046919bbed9
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2019-12-04 14:30:58 +0000

    Refactored tests (wip)

--- core/test/views/login/login_test.dart
+++ core/test/views/login/login_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:components/components/input_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_core/authentication/bloc/bloc.dart';
 import 'package:flash_chat_core/repositories/user_repository.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import '../../test_debugger_bloc_delegate.dart';
 
 class LoginWrapper extends StatelessWidget {
   const LoginWrapper();
@@ -29,6 +31,10 @@ class LoginWrapper extends StatelessWidget {
 }
 
 void main() {
+  final testDebuggerBlocDelegate = TestDebuggerBlocDelegate();
+
+  BlocSupervisor.delegate = testDebuggerBlocDelegate;
+
   final firebaseAuth = FirebaseAuth.instance;
 
   final flutterSecureStorage = FlutterSecureStorage();
@@ -54,11 +60,108 @@ void main() {
     );
   }
 
-  testWidgets('builds widget', (WidgetTester tester) async {
+  testWidgets('builds widget with no event or state',
+      (WidgetTester tester) async {
     await tester.pumpWidget(buildLogin());
 
     await tester.pump();
 
     expect(find.byType(Login), findsOneWidget);
+    expect(testDebuggerBlocDelegate.lastEvent, null);
+    expect(testDebuggerBlocDelegate.currentState, null);
+    expect(testDebuggerBlocDelegate.nextState, null);
+  });
+
+  testWidgets('login tap with no input does not call event or change state',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(buildLogin());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Login'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, null);
+    expect(testDebuggerBlocDelegate.currentState, null);
+    expect(testDebuggerBlocDelegate.nextState, null);
+  });
+
+  testWidgets('email input calls event and changes state',
+      (WidgetTester tester) async {
+    emailInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter email';
+
+    await tester.pumpWidget(buildLogin());
+
+    await tester.pump();
+
+    await tester.enterText(find.byWidgetPredicate(emailInputField), 'test');
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'LoginChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'LoginInitial');
+    expect(testDebuggerBlocDelegate.nextState, 'LoginFillInProgress');
+  });
+
+  testWidgets('password input calls event and changes state',
+      (WidgetTester tester) async {
+    passwordInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter password';
+
+    await tester.pumpWidget(buildLogin());
+
+    await tester.pump();
+
+    await tester.enterText(find.byWidgetPredicate(passwordInputField), 'test');
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'LoginChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'LoginInitial');
+    expect(testDebuggerBlocDelegate.nextState, 'LoginFillInProgress');
+  });
+
+  testWidgets('correct input calls event and changes state',
+      (WidgetTester tester) async {
+    emailInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter email';
+
+    passwordInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter password';
+
+    await tester.pumpWidget(buildLogin());
+
+    await tester.pump();
+
+    await tester.enterText(
+        find.byWidgetPredicate(emailInputField), 'test@email.com');
+
+    await tester.enterText(
+        find.byWidgetPredicate(passwordInputField), 'Abcde12345@');
+
+    await tester.pump();
+
+    await tester.tap(find.text('Login'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'LoginChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'LoginFillInProgress');
+    expect(testDebuggerBlocDelegate.nextState, 'LoginFillSuccess');
+  });
+
+  testWidgets('cancel tap calls event and does not change state',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(buildLogin());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Cancel'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'CancelButtonPressed');
   });
 }
--- core/test/views/register/register_test.dart
+++ core/test/views/register/register_test.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:components/components/input_field.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat_core/authentication/bloc/bloc.dart';
 import 'package:flash_chat_core/repositories/user_repository.dart';
@@ -10,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import '../../test_debugger_bloc_delegate.dart';
 
 class RegisterWrapper extends StatelessWidget {
   const RegisterWrapper();
@@ -29,6 +31,10 @@ class RegisterWrapper extends StatelessWidget {
 }
 
 void main() {
+  final testDebuggerBlocDelegate = TestDebuggerBlocDelegate();
+
+  BlocSupervisor.delegate = testDebuggerBlocDelegate;
+
   final firebaseAuth = FirebaseAuth.instance;
 
   final flutterSecureStorage = FlutterSecureStorage();
@@ -54,11 +60,108 @@ void main() {
     );
   }
 
-  testWidgets('builds widget', (WidgetTester tester) async {
+  testWidgets('builds widget with no event or state',
+      (WidgetTester tester) async {
     await tester.pumpWidget(buildRegister());
 
     await tester.pump();
 
     expect(find.byType(Register), findsOneWidget);
+    expect(testDebuggerBlocDelegate.lastEvent, null);
+    expect(testDebuggerBlocDelegate.currentState, null);
+    expect(testDebuggerBlocDelegate.nextState, null);
+  });
+
+  testWidgets('Register tap with no input does not call event or change state',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(buildRegister());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Register'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, null);
+    expect(testDebuggerBlocDelegate.currentState, null);
+    expect(testDebuggerBlocDelegate.nextState, null);
+  });
+
+  testWidgets('email input calls event and changes state',
+      (WidgetTester tester) async {
+    emailInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter email';
+
+    await tester.pumpWidget(buildRegister());
+
+    await tester.pump();
+
+    await tester.enterText(find.byWidgetPredicate(emailInputField), 'test');
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'RegisterChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'RegisterInitial');
+    expect(testDebuggerBlocDelegate.nextState, 'RegisterFillInProgress');
+  });
+
+  testWidgets('password input calls event and changes state',
+      (WidgetTester tester) async {
+    passwordInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter password';
+
+    await tester.pumpWidget(buildRegister());
+
+    await tester.pump();
+
+    await tester.enterText(find.byWidgetPredicate(passwordInputField), 'test');
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'RegisterChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'RegisterInitial');
+    expect(testDebuggerBlocDelegate.nextState, 'RegisterFillInProgress');
+  });
+
+  testWidgets('correct input calls event and changes state',
+      (WidgetTester tester) async {
+    emailInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter email';
+
+    passwordInputField(Widget widget) =>
+        widget is InputField && widget.hintText == 'Enter password';
+
+    await tester.pumpWidget(buildRegister());
+
+    await tester.pump();
+
+    await tester.enterText(
+        find.byWidgetPredicate(emailInputField), 'test@email.com');
+
+    await tester.enterText(
+        find.byWidgetPredicate(passwordInputField), 'Abcde12345@');
+
+    await tester.pump();
+
+    await tester.tap(find.text('Register'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'RegisterChanged');
+    expect(testDebuggerBlocDelegate.currentState, 'RegisterFillInProgress');
+    expect(testDebuggerBlocDelegate.nextState, 'RegisterFillSuccess');
+  });
+
+  testWidgets('cancel tap calls event and does not change state',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(buildRegister());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Cancel'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'CancelButtonPressed');
   });
 }
