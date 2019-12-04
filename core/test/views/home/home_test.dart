dfb4243c19e69eca445a2d903dc3c324b1b807c9
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2019-12-04 13:58:21 +0000

    Added tests

--- core/test/views/home/home_test.dart
+++ core/test/views/home/home_test.dart
@@ -1,8 +1,10 @@
+import 'package:bloc/bloc.dart';
 import 'package:flash_chat_core/views/home/bloc/bloc.dart';
 import 'package:flash_chat_core/views/home/home.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import '../../test_debugger_bloc_delegate.dart';
 
 class HomeWrapper extends StatelessWidget {
   const HomeWrapper();
@@ -22,6 +24,10 @@ class HomeWrapper extends StatelessWidget {
 }
 
 void main() {
+  final testDebuggerBlocDelegate = TestDebuggerBlocDelegate();
+
+  BlocSupervisor.delegate = testDebuggerBlocDelegate;
+
   Widget buildHome() {
     return BlocProvider(
       create: (BuildContext context) => HomeBloc(),
@@ -35,5 +41,29 @@ void main() {
     await tester.pump();
 
     expect(find.byType(Home), findsOneWidget);
+  });
+
+  testWidgets('login tap calls event', (WidgetTester tester) async {
+    await tester.pumpWidget(buildHome());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Login'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'LoginButtonPressed');
+  });
+
+  testWidgets('register tap calls event', (WidgetTester tester) async {
+    await tester.pumpWidget(buildHome());
+
+    await tester.pump();
+
+    await tester.tap(find.text('Register'));
+
+    await tester.pump();
+
+    expect(testDebuggerBlocDelegate.lastEvent, 'RegisterButtonPressed');
   });
 }
