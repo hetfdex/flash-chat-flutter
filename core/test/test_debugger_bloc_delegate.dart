2bba4097bc6dd1cb67df65a4e3d8e581ef88fb28
Jose Pereira <jose.pedro.pereira@criticaltechworks.com>
2019-12-04 14:30:25 +0000

    Added new helper method

--- core/test/test_debugger_bloc_delegate.dart
+++ core/test/test_debugger_bloc_delegate.dart
@@ -2,10 +2,20 @@ import 'package:bloc/bloc.dart';
 
 class TestDebuggerBlocDelegate extends BlocDelegate {
   String lastEvent;
+  String currentState;
+  String nextState;
 
   @override
   void onEvent(Bloc<dynamic, dynamic> bloc, Object event) {
     super.onEvent(bloc, event);
     lastEvent = event.toString();
+  }
+
+  @override
+  void onTransition(
+      Bloc<dynamic, dynamic> bloc, Transition<dynamic, dynamic> transition) {
+    super.onTransition(bloc, transition);
+    currentState = transition.currentState.toString();
+    nextState = transition.nextState.toString();
   }
 }
